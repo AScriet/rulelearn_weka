7f7926a4ddd8e6d4cefea7414cac41655a354955
AScriet <stepanyuk02alist.ru@gmail.com>
2023-12-08 20:31:25 +0100

    update DRSA classification

--- src/main/java/weka/classifiers/rules/DRSA.java
+++ src/main/java/weka/classifiers/rules/DRSA.java
@@ -3,6 +3,8 @@ package weka.classifiers.rules;
 import org.rulelearn.approximations.*;
 import org.rulelearn.data.InformationTable;
 import org.rulelearn.data.InformationTableWithDecisionDistributions;
+import org.rulelearn.rules.Rule;
+import org.rulelearn.rules.RuleSetWithCharacteristics;
 import org.rulelearn.measures.dominance.EpsilonConsistencyMeasure;
 import org.rulelearn.rules.*;
 import org.data.ArffConverter;
@@ -11,19 +13,18 @@ import weka.classifiers.AbstractClassifier;
 import weka.core.*;
 import org.rulelearn.approximations.VCDominanceBasedRoughSetCalculator;
 
-
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.Vector;
+import java.util.*;
 
 public class DRSA extends AbstractClassifier implements
         TechnicalInformationHandler {
 
     String unionsS = "";
-    String Rules = "";
-    int rulesNum;
     private double Th = 0.0;
-    RuleSetWithCharacteristics resultSetModel;
+    protected int instanceNum = 0;
+    protected int batchSize = 0;
+    private ArrayList<String> decisions;
+    protected transient RuleSetWithCharacteristics resultSetModel = null;
+    protected transient InformationTableWithDecisionDistributions informationTableWithDecisionDistributions = null;
     public String globalInfo(){
         return "test info about DRSA" + getTechnicalInformation().toString();
     }
@@ -54,7 +55,6 @@ public class DRSA extends AbstractClassifier implements
         } else {
             Th = 0.0;
         }
-        //System.out.println("XYU");
         super.setOptions(options);
     }
 
@@ -74,32 +74,26 @@ public class DRSA extends AbstractClassifier implements
 
 
         InformationTable informationTable;
-        ArffConverter arffConverter = new ArffConverter();
         ArffInstance2Table arffC = new ArffInstance2Table();
 
         informationTable = arffC.convert(data);
-        //informationTable = arffConverter.convert(data.toString());
 
-
-        for (int i = 0; i < informationTable.getNumberOfAttributes(); i++) {
-            //System.out.println(informationTable.getAttribute(i).serialize());
-        }
-        //System.out.println(Arrays.toString(informationTable.getFields(0)));
-        //System.out.println(informationTable.getDecision(0));
-        InformationTableWithDecisionDistributions informationTableWithDecisionDistributions = new InformationTableWithDecisionDistributions(informationTable);
-
-        //System.out.println(informationTableWithDecisionDistributions.getDominanceConesDecisionDistributions().getNumberOfObjects());
-        for (int i = 0; i < 10; i++) {
-            //System.out.println(informationTableWithDecisionDistributions.getDecision(i));
+        batchSize = data.size();
+        /* enum class values */
+        int n = data.numClasses();
+        decisions = new ArrayList<String>();
+        for (int i = 0; i < n; i++) {
+            decisions.add(data.classAttribute().value(i));
         }
 
+        informationTableWithDecisionDistributions = new InformationTableWithDecisionDistributions(informationTable);
 
-        RuleSetWithComputableCharacteristics rules = null;
+        RuleSetWithComputableCharacteristics rules;
         //UnionsWithSingleLimitingDecision unions = new UnionsWithSingleLimitingDecision(
         //        informationTableWithDecisionDistributions,
         //        new VCDominanceBasedRoughSetCalculator(RoughMembershipMeasure.getInstance(), 0.0));
 
-        UnionsWithSingleLimitingDecision unions = null;
+        UnionsWithSingleLimitingDecision unions;
 
 
 
@@ -107,7 +101,7 @@ public class DRSA extends AbstractClassifier implements
                 informationTableWithDecisionDistributions,
                 new VCDominanceBasedRoughSetCalculator(EpsilonConsistencyMeasure.getInstance(), Th));
 
-        RuleInducerComponents ruleInducerComponents = null;
+        RuleInducerComponents ruleInducerComponents;
 
         ApproximatedSetProvider unionAtLeastProvider = new UnionProvider(Union.UnionType.AT_LEAST, unions);
         ApproximatedSetProvider unionAtMostProvider = new UnionProvider(Union.UnionType.AT_MOST, unions);
@@ -136,35 +130,8 @@ public class DRSA extends AbstractClassifier implements
         resultSet = RuleSetWithCharacteristics.join(resultSet, rules);
 
         resultSet.setLearningInformationTableHash(unions.getInformationTable().getHash());
-        //System.out.println("CLASS UNIONS: ");
-        //System.out.println(unionAtLeastProvider.getCount());
-        /**
-        for (int i = 0; i < unionAtLeastProvider.getCount(); i++) {
-            System.out.println(unionAtLeastProvider.getApproximatedSet(i) + " "
-                    + unionAtLeastProvider.getApproximatedSet(i).getAccuracyOfApproximation() + " "
-                    + unionAtLeastProvider.getApproximatedSet(i).getQualityOfApproximation());
-        }
-        for (int i = 0; i < unionAtMostProvider.getCount(); i++) {
-            System.out.println(unionAtMostProvider.getApproximatedSet(i) + " "
-                    + unionAtMostProvider.getApproximatedSet(i).getAccuracyOfApproximation() + " "
-                    + unionAtMostProvider.getApproximatedSet(i).getQualityOfApproximation());
-        }
-         */
-        //System.out.println("Number of rules: " + resultSet.size());
-        //System.out.println("RULES: ");
-        //System.out.println(resultSet.serialize());
-        int count = 0;
-        for (int i = 0; i < informationTable.getNumberOfObjects(); i++) {
-            if (resultSet.getRule(0).covers(i, informationTable)){
-                count ++;
-            };
-        }
-        rulesNum = resultSet.size();
-        for (int i = 0; i < rulesNum; i++) {
-            Rules += resultSet.getRule(i) + "\n";
-        }
-        //System.out.println(count);
-        //Unions unions = new Unions(new VCDominanceBasedRoughSetCalculator(RoughMembershipMeasure.getInstance(), 0.1))
+
+
 
         for (int i = 0; i < unionAtLeastProvider.getCount(); i++) {
             unionsS +=(unionAtLeastProvider.getApproximatedSet(i) + " "
@@ -176,19 +143,35 @@ public class DRSA extends AbstractClassifier implements
                     + unionAtMostProvider.getApproximatedSet(i).getAccuracyOfApproximation() + " "
                     + unionAtMostProvider.getApproximatedSet(i).getQualityOfApproximation() + "\n");
         }
+
         resultSetModel = resultSet;
 
     }
 
     @Override
     public double classifyInstance(Instance instance) throws Exception {
-        //System.out.println(instance);
-        for (int i = 0; i < resultSetModel.size(); i++){
-            resultSetModel.getRule(i);
+        String ruleStr;
+        double covers = 1;
+        for (int i = 0; i < resultSetModel.size(); i++) {
+            Rule rule = resultSetModel.getRule(i);
+            if(rule.covers(instanceNum, informationTableWithDecisionDistributions)) {
+                ruleStr = rule.getDecision().toString();
+                for (int j = 0; j < instance.numClasses(); j++){
+                    if (ruleStr.contains(decisions.get(j))){
+                        covers = j;
+                    }
+                }
+                break;
+            }
+
         }
-        System.out.println(resultSetModel.getRule(0));
-        return 1;
+        instanceNum += 1;
+        if (instanceNum == batchSize)
+            instanceNum = 0;
+
+        return covers;
     }
+
 
     @Override
     public String toString() {
@@ -197,12 +180,9 @@ public class DRSA extends AbstractClassifier implements
 
         text.append("\nCLASS UNIONS: \n ===========\n");
         text.append(unionsS);
-        //text.append("Number of rules: " + resultSet.size() + "\n");
-        //text.append("RULES: ");
-        //text.append(resultSet.serialize());
         text.append("\nDRSA rules: \n ===========\n");
-        text.append(Rules);
-        text.append("\nNum of Rules: " + rulesNum + "\n");
+        text.append(resultSetModel.serialize("\n"));
+        text.append("\nNum of Rules: " + resultSetModel.size() + "\n");
         return text.toString();
     }
 
